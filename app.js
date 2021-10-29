3a91209a92e97092bb4e43b9970d49efed813fb0
Ifeanyichukwu John <ifeanyichukwujohn70@gmail.com>
2021-10-29 10:11:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let start = document.querySelector(".start-btn")
+let stop = document.querySelector(".stop-btn")
+var player = document.querySelector(".videoPlayer");
+var download = document.querySelector(".download-btn");
+
+start.addEventListener("click", async function () {
+    let stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true
+    });
+
+    mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "video/webm"
+    })
+    
+    mediaRecorder.start()
+    let chunks = []
+    mediaRecorder.addEventListener('dataavailable', function (e) {
+        chunks.push(e.data)
+        //console.log(e.data);
+    });
+
+
+    mediaRecorder.addEventListener('stop', function () {
+        stop.style.display = "none";
+        let blob = new Blob(chunks, {
+            type: chunks[0].type
+        });
+        let url = URL.createObjectURL(blob);
+        //console.log(url);
+
+        let video = document.querySelector("video")
+        video.src = url
+        player.style.display = "";
+        video.play();
+        download.href = url
+        download.download = 'screenRecord.webm';
+        download.style.display = "";
+
+
+        stop.addEventListener("click", function () {
+            mediaRecorder.stop();
+            stop.style.display = "none";
+        });
+    })
+})
